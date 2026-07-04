8b3460def9447314bef88453df14e5fca5d094ac
spl67ii <sairaleshreddy.pelleti@colruytgroup.com>
2026-07-04 09:17:55 +0200

    Add CORS support to RecipeAiController for local development

--- src/main/java/com/colruytgroup/intelligentpantry/controller/RecipeAiController.java
+++ src/main/java/com/colruytgroup/intelligentpantry/controller/RecipeAiController.java
@@ -3,12 +3,14 @@ package com.colruytgroup.intelligentpantry.controller;
 import com.colruytgroup.intelligentpantry.ai.RecipeAiService;
 import com.colruytgroup.intelligentpantry.dto.response.RecipeRecommendationResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/ai")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class RecipeAiController {
 
